1357a07a96f5e0b3986cd4d29140f3ab41aa7edd
Kalick <kalick95@gmail.com>
2021-12-29 01:37:00 +0100

    connection done

--- src/main/java/com/library/App.java
+++ src/main/java/com/library/App.java
@@ -1,13 +1,32 @@
 package com.library;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.*;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
+
 {
-    public static void main( String[] args )
+    static Configuration conf = HBaseConfiguration.create();
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        conf.set("hbase.zookeeper.quorum","zoo-1.au.adaltas.cloud");
+        conf.set("hbase.zookeeper.property.clientPort","2181");
+
+        Connection connection = ConnectionFactory.createConnection(conf);
+        Table library = connection.getTable(TableName.valueOf("ece_2021_fall_app_1:library"));
+
+        Scan scan1 = new Scan();
+        ResultScanner scanner1 = library.getScanner(scan1);
+
+        for (Result res : scanner1) {
+            System.out.println(res);
+        }
     }
 }
